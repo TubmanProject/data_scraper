6b9cf43576ade2bbc0bb5a9a7c3f14e40bef1640
tyronemsaunders <tyrone.saunders@yahoo.com>
2018-01-12 03:10:49 -0600

    Added route and templates for html jupyter notebooks generated with jupyter nbconvert.

--- app/frontend/controllers.py
+++ app/frontend/controllers.py
@@ -1,4 +1,7 @@
 """Controllers module for the frontend package."""
+import os
+
+from app.constants import FLASK_PROJECT_ROOT
 from flask import Blueprint, render_template
 
 frontend = Blueprint('frontend', __name__)
@@ -8,3 +11,21 @@ frontend = Blueprint('frontend', __name__)
 def api_index():
     """Index page for the API documentation."""
     return render_template('api_docs.html')
+
+
+@frontend.route('/notebooks', methods=['GET'])
+def notebook_index():
+    """Show a list of notebooks."""
+    # get the html files from the templates
+    jupyter_notebooks = os.listdir(os.path.join(FLASK_PROJECT_ROOT, 'templates', 'jupyter_notebooks'))
+    html_files = [i for i in jupyter_notebooks if i.endswith('.html')]
+    html_file_roots = [os.path.splitext(i)[0] for i in html_files]
+
+    # render the templates
+    return render_template('jupyter_notebooks.html', notebooks=html_file_roots)
+
+
+@frontend.route('/notebooks/<notebook_name>', methods=['GET'])
+def jupyter_notebook(notebook_name):
+    """Render Jupyter Notebooks."""
+    return render_template("jupyter_notebooks/{}.html".format(notebook_name))
